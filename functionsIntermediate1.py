f423b97727803a8c33041835faf5d59764afed5f
Chris <cmsl4y3r@gmail.com>
2021-07-08 17:28:51 -0600

    Reworked and fixed for some of the tests

--- functionsIntermediate1.py
+++ functionsIntermediate1.py
@@ -1,11 +1,13 @@
 import random
 def randInt(min=0  , max=100   ):
     if(max>min and max > 0):
-        num = random.random()*max+min
+        num = int(random.random()*(max-min)+min)
+        print("max = " + str(max))
+        print("min = " + str(min))
         return num
     else:
         print("Please make sure that max is greater than zero and greater than minimum")
-print(randInt()) 		
+print(randInt(max=-1)) 		
 # should print a random integer between 0 to 100
 print(randInt(max=50)) 
 # should print a random integer between 0 to 50
